14abca9a2ce9b23a49d9b2bbc10df8ef28fb048f
Max Oliveira <max-oliveira@outlook.com.br>
2023-05-30 17:13:06 -0300

    create telas sign e preload

---
+++ src/screens/Preload/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Container} from './styles';
+import {Text} from 'react-native';
+
+export default () => {
+  return <Container />;
+};
---
+++ src/screens/Preload/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.view`
+  background-color: #63c2d1;
+  flex: 1;
+  justify-content:center
+  align-items: center
+  `;
